df32640583221f39ff8ac38fba60282b40546d1a
Sebastian Abondano <sabondano1@gmail.com>
2015-09-14 13:50:10 -0600

    First commit
    Why:
    
    * To kickoff the project.
    
    This change addresses the need by:
    
    * Creating a new rails project with rspec and postgres.
    * Setting up DatabaseCleaner
    * Setting up backtrace silencer

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+RSpec.configure do |config|
+  config.backtrace_exclusion_patterns << %r{/gems/}
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+end
