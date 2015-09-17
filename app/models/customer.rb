8b53fc27652334d3e526ea43853b5c1ffd3346ab
Sebastian Abondano <sabondano1@gmail.com>
2015-09-16 23:57:19 -0600

    Refactor #favorite_merchant in Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,12 +3,16 @@ class Customer < ActiveRecord::Base
   has_many :transactions, through: :invoices
 
   def favorite_merchant
-    Merchant.find(invoices
-                    .paid
-                    .group(:merchant_id)
-                    .count.sort_by(&:last)
-                    .reverse
-                    .first
-                    .first)
+    Merchant.find(merchant_with_most_invoices(count_paid_invoices_grouped_by_merchant_id))
+  end
+
+  private
+
+  def count_paid_invoices_grouped_by_merchant_id
+    invoices.paid.group(:merchant_id).count
+  end
+
+  def merchant_with_most_invoices(invoices)
+    invoices.sort_by { |merchant_id, count| count }.last[0]
   end
 end
